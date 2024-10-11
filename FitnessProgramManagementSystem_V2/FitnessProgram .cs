e11835ea01172cd63202c3da5e01c706cb9dcbc3
Sacsitha M. <UT01410@unicomtic.com>
2024-10-11 12:56:27 +0530

    second commit Question 13 start

---
+++ FitnessProgramManagementSystem_V2/FitnessProgram .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    public class FitnessProgram
+    {
+        public string FitnessProgramId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+        public FitnessProgram(string fitnessProgramId, string title, string duration, decimal price)
+        {
+            FitnessProgramId = fitnessProgramId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
+        public FitnessProgram() { }
+    }
+}
---
+++ FitnessProgramManagementSystem_V2/FitnessProgramRepository .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgramRepository
+    {
+        public readonly string ConnectionString = "server=(localdb)\\MSSQLLocalDB;database=FitnessProgramManagement;";
+
+        public void AddProgram(FitnessProgram program)
+        {
+            string query = "Insert into FitnessPrograms values (@id,@Title,@duration,@price)";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using(SqlCommand command = connection.CreateCommand())
+                {
+                    
+                }
+            }
+        }
+    }
+}
